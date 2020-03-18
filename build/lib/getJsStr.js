9fed3984a252b49dba0c1f58c96f3b69cd1ec31a
zvakanaka <8365885+zvakanaka@users.noreply>
2020-03-17 18:57:19 -0600

    fix path for utils

--- build/lib/getJsStr.js
+++ build/lib/getJsStr.js
@@ -5,12 +5,13 @@ module.exports = function getJsStr() {
   const jsFiles = [ `${__dirname}/../../client/utils.js`, ...filesInDir('src/js', ['js']) ];
   const jsStr = jsFiles.reduce((acc, cur, i) => {
     const fileName = i ? `src/js/${cur}` : cur;
+    const displayedFileName = i ? `'${fileName}'` : `'utils.js' (helper from create-extension)`;
     const str = file.read(fileName);
     const isAsync = str.includes('await ');
     const iife = !!i;
     return `${acc}
 /*******************************************************************************
- * JS source file '${fileName}'${iife ? ' (wrapped in IIFE)' : ''}
+ * JS source file ${displayedFileName}${iife ? ' (wrapped in IIFE)' : ''}
  ******************************************************************************/
 ${iife ? `(${isAsync ? 'async ' : ''}function() {
 ` : ''}${str}${iife ? `
